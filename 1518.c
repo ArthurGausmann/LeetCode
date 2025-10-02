dd6b7ffe742a07c51b2355ef562163c7f788eb92
ArthurGausmann <arthur.campellogausmann@gmail.com>
2025-10-02 20:25:00 -0300

    1518. Resolvido

--- 1518.c
+++ 1518.c
@@ -1,17 +1,18 @@
-#include <stdio.h>
+/*
+1518.
+There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles from the market with one full water bottle.
+The operation of drinking a full water bottle turns it into an empty bottle.
+Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
+*/
 
-int main() {
-    int numBottles = 15, numExchange = 4;
-    int bebidas = 1;
-    while( numBottles != 0 ) {
-        int resto = numBottles % numExchange;
-        int divisao = (numBottles / numExchange) - 1;
-        bebidas += numBottles;
-        numBottles = resto + divisao;
+int numWaterBottles(int numBottles, int numExchange) {
+    int bebidas = 0, vazias = 0;
+    while( numBottles >= numExchange ) {
+        int divisao = numBottles / numExchange;
+        bebidas += numExchange * divisao;
+        numBottles -= numExchange * divisao;
+        numBottles += divisao;
     }
 
-    int vazias = numBottles;
-
-    printf("Garrafas bebidas: %d\n", bebidas);
-    return bebidas;
+    return bebidas + numBottles;
 }
